d26d5566cff1e4172fd57382b0420f3746b5bf62
tea <tea@hidden.org>
2021-05-22 00:09:40 +0700

    devfreq:forgot patch

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -314,7 +314,8 @@ struct devfreq_passive_data {
 	struct notifier_block nb;
 };
 #endif
-
+/* Caution: devfreq->lock must be locked before calling update_devfreq */
+extern int update_devfreq(struct devfreq *devfreq);
 #else /* !CONFIG_PM_DEVFREQ */
 static inline struct devfreq *devfreq_add_device(struct device *dev,
 					  struct devfreq_dev_profile *profile,
@@ -420,6 +421,12 @@ static inline int devfreq_update_stats(struct devfreq *df)
 {
 	return -EINVAL;
 }
+
+static inline int update_devfreq(struct devfreq *devfreq)
+{
+	return -EINVAL;
+}
+
 #endif /* CONFIG_PM_DEVFREQ */
 
 #endif /* __LINUX_DEVFREQ_H__ */
